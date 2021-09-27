8becf0a280f0a25bfb0f62dfe47912fbb19ca92f
Romulo Brandão <42874556+itsRomulo@users.noreply.github.com>
2021-09-27 12:55:13 -0300

    Add files via upload
    Mover Arquivos via python

---
+++ movefile.py
@@ -0,0 +1,35 @@
+import shutil
+import os
+import os.path
+import datetime
+from datetime import datetime
+from datetime import date
+
+def move_arq(arquivo,dir,de,para):
+    try:
+        status='OK'
+        dir_ori=os.path.join(dir,de)
+        dir_des=os.path.join(dir,para)
+        if len(arquivo)>0:
+            file_name=arquivo
+            shutil.move(os.path.join(dir_ori, file_name), dir_des)
+        else:
+            file_names = os.listdir(dir_ori)
+            for file_name in file_names:
+                shutil.move(os.path.join(dir_ori, file_name), dir_des)
+    except Exception as e:
+        status='NOK' 
+    return status
+
+data_hoje = datetime.now().strftime('%Y%m%d')
+
+directory = 'C:\\'
+
+de = 'testebat\\'
+
+para = 'testebat2\\'
+
+arquivo = data_hoje+'date.txt'
+print(directory+de+arquivo)
+#print(data_hoje)
+move_arq(arquivo,directory,de,para)
