7eb8dc827bc396c381c407d9612ee10a443293ee
Romulo Brandão <42874556+itsRomulo@users.noreply.github.com>
2022-03-03 12:16:23 -0300

    Update settings.py

--- django/teladelogin/tutorialcadastro/settings.py
+++ django/teladelogin/tutorialcadastro/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "g(ugnz#7lb$jbex0q4vtc=uh@do#(zru_qzk+z!@ll@yeg=k#y"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
